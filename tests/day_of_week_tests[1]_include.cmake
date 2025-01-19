05ce6f053d448651a9b98d0f3ae716eb03c84b3d
manith-hetti <76125387+manith-hetti@users.noreply.github.com>
2025-01-19 10:47:39 +0000

    removed some .gitignore files

--- tests/day_of_week_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/manith-hetti/Documents/Projects/dayofweek/tests/day_of_week_tests[1]_tests.cmake")
-  include("/home/manith-hetti/Documents/Projects/dayofweek/tests/day_of_week_tests[1]_tests.cmake")
-else()
-  add_test(day_of_week_tests_NOT_BUILT day_of_week_tests_NOT_BUILT)
-endif()
